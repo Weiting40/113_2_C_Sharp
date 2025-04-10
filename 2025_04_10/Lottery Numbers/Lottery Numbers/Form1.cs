f2ad62d677b067b4eed98ff35fff67c23517d988
Weiting40 <a113221040@mail.shu.edu.tw>
2025-04-10 13:04:39 +0800

    2025_04_10

---
+++ 2025_04_10/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 5;//產生5個隨機數字
+            int[]lotteryNumbers= new int[SIZE];//宣告一個陣列儲存樂透號碼
+            Random random = new Random();//創建一個Random物件，用於生成隨機數
+
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                int number;
+                do
+                {
+                    number = random.Next(1, 43);//產生1~42的隨機數字
+                } while (lotteryNumbers.Contains(number));//檢查是否已經存在於陣列中
+                lotteryNumbers[i] = number;//將隨機數字存入陣列
+            }
+            //將lotteryNumbers陣列中的數字由小到大排序
+            Array.Sort(lotteryNumbers);//使用Array.Sort方法將lotteryNumbers陣列中的數字進行排序
+            //註解掉的原始程式碼，逐一將樂透號碼顯示在對應的標籤上
+            //firstLabel.Text = lotteryNumbers[0].ToString();
+            //secondLabel.Text = lotteryNumbers[1].ToString();
+            //thirdLabel.Text = lotteryNumbers[2].ToString();
+            //fourthLabel.Text = lotteryNumbers[3].ToString();
+            //fifthLabel.Text = lotteryNumbers[4].ToString();
+
+            Label[]showLabels = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                showLabels[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 2025_04_10/Test Average/Test Average/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Highest(int[] scores)
+        {
+            int highScore = [0];
+            foreach (int score in scores)
+            {
+                if (score > highScore)
+                {
+                    highScore = score;
+                }
+            }
+        }
+        private double Average(int[] scores)
+        {
+            double total = 0.0;
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+            return total / scores.Length;
+        }
+        private int Lowest( int[] scores  )
+        {
+            int lowScore = [0];
+            foreach (int score in scores)
+            {
+                if (score < lowScore)
+                {
+                    lowScore = score;
+                }
+            }
+        }
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 48;
+            int[] testScores = new int[SIZE];
+            int index = 0;
+            int highScore = 0;
+            int lowScore = 100;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            {
+                if(openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    while (!inputFile.EndOfStream)
+                    {
+                        testScores[index] = Convert.ToInt32(inputFile.ReadLine());
+                        index++;
+                    }
+                    inputFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No file selected.", "Error");
+                }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
